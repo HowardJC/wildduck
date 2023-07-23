49ec326ce5f5e65f11876f3a06e22788bbcf0904
HowardCJ <joshua.howard@meetkai.com>
2023-07-23 07:43:44 -0700

    TheBest

--- setup/11_install_nginx.sh
+++ setup/11_install_nginx.sh
@@ -55,6 +55,18 @@ echo "server {
         proxy_redirect off;
     }
 
+    # special config for /mail/api path
+    location /mail/api/ {
+        rewrite ^/mail/api/(.*) /$1 break;
+        proxy_http_version 1.1;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header HOST \$http_host;
+        proxy_set_header X-NginX-Proxy true;
+        proxy_pass http://127.0.0.1:8080;
+        proxy_redirect off;
+    }
+
     location / {
         proxy_http_version 1.1;
         proxy_set_header X-Real-IP \$remote_addr;
